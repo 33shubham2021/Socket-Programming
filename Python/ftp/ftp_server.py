51d38ab7f52f0fd0b474ee95be08b3c2fdd64501
Shubham Sharma <33shubham2021@gmail.com>
2025-11-13 11:06:09 +0530

    adding ftp server and test files/directory

--- Python/ftp/ftp_server.py
+++ Python/ftp/ftp_server.py
@@ -2,6 +2,10 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+#"C:\Users\33shu\Documents\Coding\FTP-Practice_Directory\user1"
+
+COMPANY_PATH = "./FTP-Practice_Directory/user1"
+BANK_PATH = "./FTP-Practice_Directory/user2"
 
 IP = "127.0.0.1"
 PORT = 21
@@ -9,7 +13,9 @@ PORT = 21
 authorizer = DummyAuthorizer()
 
 # full permissions for user=user and password=12345
-authorizer.add_user("user", "12345", ".", perm="elradfmw")  
+authorizer.add_user("company", "12345", COMPANY_PATH, perm="elradfmw")
+authorizer.add_user("bank", "123456", BANK_PATH, perm="elradfmw")
+
 
 handler = FTPHandler
 handler.authorizer = authorizer
